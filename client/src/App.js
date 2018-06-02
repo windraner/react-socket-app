ec0b294f5898c1d2029fbe1a0a4557582022ea72
windraner <c1net@rambler.ru>
2018-06-02 18:11:19 +0300

    'second'

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Customers from './components/customers';
 
 class App extends Component {
   render() {
@@ -9,9 +8,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React Express Starter</h1>
+          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <Customers />
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code>
+        </p>
       </div>
     );
   }
